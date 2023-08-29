659679177aa5e817f35d4037c4c384f5d3bb6b16
Nik Kstchv <telerific@gmail.com>
2023-08-29 16:56:01 +0300

    add hero

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-wrap py-2">
+  <div class="flex flex-wrap py-2 w-full fixed border-b">
     <div class="w-full px-4">
       <nav
         class="relative flex flex-wrap items-center justify-between px-2 py-3 rounded"
@@ -11,32 +11,32 @@
             <ul class="flex flex-col lg:flex-row list-none lg:mr-auto">
               <li class="nav-item">
                 <a
-                  class="px-3 py-2 flex items-center text-sm font-bold leading-snug text-neutral-600 hover:opacity-75"
-                  href="#pablo"
+                  class="px-3 py-1 flex items-center text-sm font-bold leading-snug text-neutral-600 hover:opacity-75 border-r-2"
+                  href="#"
                 >
                   Как это работает
                 </a>
               </li>
               <li class="nav-item">
                 <a
-                  class="px-3 py-2 flex items-center text-sm font-bold leading-snug text-neutral-600 hover:opacity-75"
-                  href="#pablo"
+                  class="px-3 py-1 flex items-center text-sm font-bold leading-snug text-neutral-600 hover:opacity-75 border-r-2"
+                  href="#"
                 >
                   Тарифы
                 </a>
               </li>
               <li class="nav-item">
                 <a
-                  class="px-3 py-2 flex items-center text-sm font-bold leading-snug text-neutral-600 hover:opacity-75"
-                  href="#pablo"
+                  class="px-3 py-1 flex items-center text-sm font-bold leading-snug text-neutral-600 hover:opacity-75 border-r-2"
+                  href="#"
                 >
                   FAQ
                 </a>
               </li>
               <li class="nav-item">
                 <a
-                  class="px-3 py-2 flex items-center text-sm font-bold leading-snug text-neutral-600 hover:opacity-75"
-                  href="#pablo"
+                  class="px-3 py-1 flex items-center text-sm font-bold leading-snug text-neutral-600 hover:opacity-75"
+                  href="#"
                 >
                   Примеры
                 </a>
@@ -47,14 +47,14 @@
             class="w-full relative flex justify-between lg:w-auto px-4 lg:static lg:block lg:justify-start"
           >
             <a
-              class="text-xs font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap  text-neutral-600 hover:opacity-75"
-              href="#pablo"
+              class="text-xs font-bold leading-relaxed inline-block mr-4 py-2 px-3 rounded whitespace-nowrap text-neutral-600 hover:opacity-75 hover:bg-slate-200"
+              href="#"
             >
               Войти
             </a>
             <a
-              class="text-xs font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap  text-neutral-600 hover:opacity-75"
-              href="#pablo"
+              class="text-xs font-bold leading-relaxed inline-block mr-4 py-2 px-3 rounded whitespace-nowrap text-neutral-600 hover:opacity-75 hover:bg-slate-200"
+              href="#"
             >
               Регистрация
             </a>
--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,55 @@
 <template>
-  <section class="dark:bg-zinc-950 dark:text-white h-screen">
+  <section
+    class="dark:bg-zinc-950 dark:text-white h-screen flex items-center justify-center container text-center flex-col"
+  >
+    <div class="text-[3rem] font-bold">PlayText</div>
+    <div class="text-sm font-semibold leading-snug text-neutral-600">Онлайн генератор текста и изображений.</div>
+    <div class="text-sm font-semibold leading-snug text-neutral-600">Мгновенно, качественно, уникально!</div>
+
     <div class="flex items-center justify-center container text-center py-10">
-      <FormKit type="multi-step" outer-class="border-0">
+      <FormKit
+        type="multi-step"
+        tab-style="progress"
+        :hide-progress-labels="true"
+        steps-class="border-0"
+      >
         <FormKit type="step" name="stepOne" next-label="Продолжить">
           <FormKit
             type="text"
-            label="Вопрос (1 из 3): О чем текст?"
+            label="Для чего вы хотите сгенерировать текст?"
             validation="required"
           />
         </FormKit>
-        <FormKit type="step" name="stepTwo">
-          <FormKit type="textarea" label="Your story" validation="required" />
-        </FormKit>
-        <FormKit type="step" name="stepThree">
+        <FormKit
+          type="step"
+          name="stepTwo"
+          next-label="Продолжить"
+          previous-label="Назад"
+        >
           <FormKit
             type="textarea"
-            label="Something else"
+            label="Какой размер текста вам необходим?"
             validation="required"
           />
+        </FormKit>
+        <FormKit
+          type="step"
+          name="stepThree"
+          next-label="Продолжить"
+          previous-label="Назад"
+        >
+          <FormKit
+            type="textarea"
+            label="Спасибо! А теперь напишите, какой текст мне для вас создать?"
+            validation="required"
+          />
+          <template #stepNext>
+            <FormKit type="submit" label="Отправить" />
+          </template>
         </FormKit>
       </FormKit>
     </div>
   </section>
 </template>
+
+<style></style>
